662fbc143118d86244e1c578b22ad0777d82008a
Tim Caswell <tim@creationix.com>
2014-07-24 23:01:41 -0500

    Move config to own file

--- samples/node-mount.js
+++ samples/node-mount.js
@@ -3,7 +3,6 @@ var modes = require('js-git/lib/modes');
 var bodec = require('bodec');
 var http = require('http');
 var wheaty = require('../wheaty');
-var jsRuntime = require('wheaty-js-runtime');
 var pathJoin = require('pathjoin');
 var run = require('gen-run');
 var sha1 = require('git-sha1');
@@ -13,22 +12,7 @@ var fetchPackProtocol = require('js-git/net/git-fetch-pack');
 var consume = require('culvert/consume');
 var configCodec = require('js-git/lib/config-codec');
 
-var repoDir = pathJoin(__dirname, "../../git");
-
-var sitesConfig = {
-  "conquest.localhost:8000": {
-    url: "https://github.com/creationix/conquest.git",
-    runtimes: { js: jsRuntime },
-  },
-  "exploder.localhost:8000": {
-    url: "https://github.com/creationix/exploder.git",
-    runtimes: { js: jsRuntime },
-  },
-  "luvit.localhost:8000": {
-    url: "git@github.com:luvit/luvit.io.git",
-    runtimes: { js: jsRuntime },
-  },
-};
+var sitesConfig = require("./sites-config");
 
 var queues = {};
 var repos = {};
@@ -62,73 +46,72 @@ function* getRepo(url) {
     queues[url].push(callback);
   }
 
+  var repo = repos[url];
+  if (repo) return repo;
+
   // Tell others we're working with it and they should wait for us.
   if (!queues[url]) queues[url] = [];
 
-  var repo = repos[url];
-  if (!repo) {
-    repo = repos[url] = {};
-    if (url[0] === "/") {
-      require('git-node-fs/mixins/fs-db')(repo, url);
+  repo = repos[url] = {};
+  if (url[0] === "/") {
+    require('git-node-fs/mixins/fs-db')(repo, url);
+  }
+  else {
+    var path = pathJoin(sitesConfig.cacheDir, sha1(url) + "-" + url.match(/[^\/]+$/)[0]);
+    require('git-node-fs/mixins/fs-db')(repo, path);
+  }
+  match = url.match(/^(https?|git):\/\/(?:([^@:]+)(?:([^@]+))?@)?([^\/:]+)(:[0-9]+)?(\/[^?]*)$/);
+  if (match) {
+    var protocol = match[1];
+    var domain = match[4];
+    var port = match[5] || defaultPorts[match[1]];
+    var name = match[6];
+    if (protocol !== "git") {
+      throw new Error("Only git:// and local files supported for now");
+    }
+    repo.transport = tcpTransport(name, domain, port);
+    require('js-git/mixins/pack-ops')(repo);
+  }
+  require('js-git/mixins/path-to-entry')(repo);
+  require('js-git/mixins/mem-cache')(repo);
+  require('js-git/mixins/formats')(repo);
+
+  var head = yield repo.readRef(ref);
+
+  if (!repo.transport) {
+    if (!head) {
+      throw new Error("No such ref in local repo " + url + "#" + ref);
+    }
+  }
+  else {
+    if (!head) {
+      console.log("Shallow clone %s at %s", url, ref);
+      yield repo.init();
+    }
+    var api = fetchPackProtocol(repo.transport);
+    var refs = yield api.take;
+    if (!(ref in refs)) {
+      throw new Error("No such ref in remote: " + ref);
+    }
+    if (refs[ref] === head) {
+      api.put(null);
+      api.put();
     }
     else {
-      var path = pathJoin(repoDir, sha1(url) + "-" + url.match(/[^\/]+$/)[0]);
-      require('git-node-fs/mixins/fs-db')(repo, path);
-    }
-    var match = url.match(/^(https?|git):\/\/(?:([^@:]+)(?:([^@]+))?@)?([^\/:]+)(:[0-9]+)?(\/[^?]*)$/);
-    if (match) {
-      var protocol = match[1];
-      var domain = match[4];
-      var port = match[5] || defaultPorts[match[1]];
-      var name = match[6];
-      if (protocol !== "git") {
-        throw new Error("Only git:// and local files supported for now");
-      }
-      repo.transport = tcpTransport(name, domain, port);
-      require('js-git/mixins/pack-ops')(repo);
-    }
-    require('js-git/mixins/path-to-entry')(repo);
-    require('js-git/mixins/mem-cache')(repo);
-    require('js-git/mixins/formats')(repo);
+      api.put({want: refs[ref]});
+      api.put({deepen: 1});
+      api.put(null);
+      api.put({done: true});
+      api.put();
 
-    var head = yield repo.readRef(ref);
-
-    if (!repo.transport) {
-      if (!head) {
-        throw new Error("No such ref in local repo " + url + "#" + ref);
-      }
+      var channels = yield api.take;
+      yield [
+        repo.unpack(channels.pack, { onProgress: write }),
+        consume(channels.progress, write),
+      ];
+      yield repo.updateRef(ref, refs[ref]);
+      yield repo.setShallow(refs[ref]);
     }
-    else {
-      if (!head) {
-        console.log("Shallow clone %s at %s", url, ref);
-        yield repo.init();
-      }
-      var api = fetchPackProtocol(repo.transport);
-      var refs = yield api.take;
-      if (!(ref in refs)) {
-        throw new Error("No such ref in remote: " + ref);
-      }
-      if (refs[ref] === head) {
-        api.put(null);
-        api.put();
-      }
-      else {
-        api.put({want: refs[ref]});
-        api.put({deepen: 1});
-        api.put(null);
-        api.put({done: true});
-        api.put();
-
-        var channels = yield api.take;
-        yield [
-          repo.unpack(channels.pack, { onProgress: write }),
-          consume(channels.progress, write),
-        ];
-        yield repo.updateRef(ref, refs[ref]);
-        yield repo.setShallow(refs[ref]);
-      }
-    }
-
   }
 
   var queue = queues[url];
@@ -207,7 +190,7 @@ var server = http.createServer(function (req, res) {
     if (!host) return;
     var handler = sites[host];
     if (!handler) {
-      var config = sitesConfig[host];
+      var config = sitesConfig.sites[host];
       if (!config) return;
       var repo = yield* getRepo(config.url);
 
@@ -217,7 +200,7 @@ var server = http.createServer(function (req, res) {
   }, function (err, result) {
     if (err) result = [500, {}, err.stack + "\n"];
     else if (!result) result = [404, {}, "Not found: " + req.url + "\n"];
-    console.log(req.method, req.url, result[0]);
+    console.log(req.method, req.headers.host, req.url, result[0]);
     res.writeHead(result[0], result[1]);
     res.end(result[2]);
   });
